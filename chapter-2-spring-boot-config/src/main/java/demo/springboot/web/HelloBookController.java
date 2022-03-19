6c2ad32d14d19b95c9d3819b9ce791dbde19c9c7
ay5219355 <1006454750@qq.com>
2022-03-19 23:41:13 +0800

    11

--- chapter-2-spring-boot-config/src/main/java/demo/springboot/web/HelloBookController.java
+++ chapter-2-spring-boot-config/src/main/java/demo/springboot/web/HelloBookController.java
@@ -1,9 +1,21 @@
 package demo.springboot.web;
 
+import com.sun.deploy.net.HttpResponse;
 import demo.springboot.config.BookProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Spring Boot Hello案例
@@ -17,8 +29,7 @@ public class HelloBookController {
     BookProperties bookProperties;
 
     @GetMapping("/book/hello")
-    public String sayHello() {
-        return "Hello， " + bookProperties.getWriter() + " is writing "
-                + bookProperties.getName() + " ！";
+    public void sayHello(@RequestParam(required = false,name = "id")String id, HttpServletResponse httpServletResponse) throws IOException {
+        Files.copy(Paths.get("E:\\github_project\\2020_PDF.pdf"),httpServletResponse.getOutputStream());
     }
 }
